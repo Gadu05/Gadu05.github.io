d8bf540774028b19824db03b2c7f88eb20b47954
Gabriel Santos <106440419+Gadu05@users.noreply.github.com>
2023-09-05 22:08:13 +0000

    teste randHiragana

---
+++ scripts/randomHiragana.js
@@ -0,0 +1,20 @@
+let hiragana;
+
+hiragana[0] = {symbol: 'あ', sound: 'A'};
+hiragana[1] = {symbol: 'い', sound: 'I'};
+hiragana[2] = {symbol: 'う', sound: 'U'};
+hiragana[3] = {symbol: 'え', sound: 'E'};
+hiragana[4] = {symbol: 'お', sound: 'O'};
+hiragana[5] = {symbol: 'か', sound: 'KA'};
+
+let arrSize = hiragana.length;
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+function returnCaractere(){
+    
+    document.getElementById("ID_RandHiragana").innerHTML = hiragana[getRandomInt(arrSize).symbol];
+
+}
