3a14ba52cd56c831a9f43ee922759a08acfdd4fb
Gabriel Eduardo de Oliveira Santos <106440419+Gadu05@users.noreply.github.com>
2024-08-20 18:24:18 +0000

    att msc 8

---
+++ scripts/musica.js
@@ -0,0 +1,15 @@
+$(function(){
+    new WOW().init();
+    //download_btn
+
+    var cvAudio = document.getElementById('cv_audio');
+    $('.cv_item').click(function(){
+        var audioLink = $(this).attr('audioLink');
+        console.log(audioLink);
+        if(audioLink){
+            cvAudio.src = audioLink;
+            cvAudio.play();
+        }
+    });
+    
+});
